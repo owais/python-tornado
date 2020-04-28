9e21cef5bb0b09fe82d2f29e8393bb5c99795671
Owais Lone <owais@lone.pw>
2020-04-29 05:02:14 +0530

    wip

--- tests/test_decorated.py
+++ tests/test_decorated.py
@@ -31,7 +31,7 @@ from .helpers.markers import (
     skip_no_async_await,
 )
 
-error_object = '<class \'ValueError\'>')
+error_object = '<class \'ValueError\'>'
 if sys.version_info.major == 2:
     error_object = '<class \'exceptions.ValueError\'>'
 
